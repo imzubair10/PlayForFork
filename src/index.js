debc588c324e92a1a917e1b942a115311da0efca
learning84 <learninglearning84@gmail.com>
2021-08-12 13:28:26 -0700

    updates index file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ console.log("world...");
 console.log("hello world...");
 
 console.log("adding contact page");
+
+// This should be the main file of the project
+// Some code needs to be written in this file for interaction
